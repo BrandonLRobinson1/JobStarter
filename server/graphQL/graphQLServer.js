cb0b5c3da02f0258fda205b702198ad651aa4a87
BrandonLRobinson1 <BrandonLRobinson1@gmail.com>
2017-05-29 23:53:13 -0700

    graphql refactor

--- server/graphQL/graphQLServer.js
+++ server/graphQL/graphQLServer.js
@@ -1,18 +1,96 @@
-//server.js
-var graphql = require ('graphql').graphql  
-var express = require('express')  
-var graphQLHTTP = require('express-graphql')  
-var Schema = require('./schema')  
+const graphql = require('graphql').graphql;
+const express = require('express');
+const graphQLHTTP = require('express-graphql');
+const SCHEMA = require('./schema');
 
-// This is just an internal test
-var query = 'query { lists { id, name, age } }'  
-graphql(Schema, query).then( function(result) {  
-  console.log(JSON.stringify(result,null," "));
+//INTERNAL TEST, NOT THE REAL DEAL
+let query = 'query { users {id,email,password} }';
+graphql(SCHEMA, query)
+  .then( function(result){
+    console.log(JSON.stringify(result, null, ' '));
+  } );
 
-});
+let app1 = express()
+  .use('/graphql', graphQLHTTP({schema: SCHEMA, pretty: true, graphiql:true}))
+  .listen(8080, function(err) {
+    console.log('GraphQL for the win')
+  })
 
-var app = express()  
-  .use('/', graphQLHTTP({ schema: Schema, pretty: true }))
-  .listen(8080, function (err) {
-    console.log('GraphQL Server is now running on localhost:8080');
-  });
+// module.exports = app1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //server.js
+// // var graphql = require ('graphql').graphql  
+// var express = require('express')  
+// var graphQLHTTP = require('express-graphql')  
+// var Schema = require('./schema')  
+
+// // This is just an internal test
+// // var query = 'query { lists { id, email, password } }'  // works
+// var query = 'query { lists { id, email, password } }'  
+// graphql(Schema, query).then( function(result) {  
+//   console.log(JSON.stringify(result,null," "));
+// });
+
+
+// // var app = express()  
+// //   .use('/graphql', req => graphQLHTTP({ 
+// //     schema: query,
+// //     rootValue: Schema,
+// //     graphiql:true,
+// //     pretty: true }))
+// //   .listen(8080, function (err) {
+// //     console.log('GraphQL Server is now running on localhost:8080');
+// //   });
+
+//   var app = express()  
+//   .use('/graphql', graphQLHTTP({ 
+//     schema: query,
+//     rootValue: Schema,
+//     graphiql:true,
+//     pretty: true }))
+//   .listen(8080, function (err) {
+//     console.log('GraphQL Server is now running on localhost:8080');
+//   });
--- server/graphQL/schema.js
+++ server/graphQL/schema.js
@@ -1,61 +1,416 @@
 var graphql = require ('graphql');
-var mongoose = require('../db/db')
+var mongoose = require('../db/db');
+// var app1 = require('./graphQLServer')
 
-// var LIST = [  
-//   {
-//     "id": 1446412739542,
-//     "name": "Read emails",
-//     "age": 23
-//   },
-//   {
-//     "id": 1446412740883,
-//     "name": "Buy orange",
-//     "age": 14
-//   }
-// ];
 
-var List = mongoose.model('Testing123', {  
-  // id: mongoose.Schema.Types.ObjectId,
-  name: String,
-  age: Number
-})
+var USERS = [  
+  {
+    "id": 1446412739542,
+    "email": "Read emails",
+    "password": 'false'
+  },
+  {
+    "id": 1446412740883,
+    "email": "Buy orange",
+    "password": 'true'
+  }
+];
 
-//defining a data type for each attribute
-var listType = new graphql.GraphQLObjectType({
-  name: 'list',
-  fields: function(){
-    return {
-      id: {
-        type: graphql.GraphQLID
+let userType = new graphql.GraphQLObjectType({
+  name: 'user',
+  fields: function() {
+    return{
+       id: {
+        type: graphql.GraphQLID //may not need or even want id
       },
-      name: {
+      email: {
         type: graphql.GraphQLString
       },
-      age: {
-        type: graphql.GraphQLInt
+      password: {
+        type: graphql.GraphQLString
       }
+      // thinking for nested fields return a new object type
     }
   }
 });
 
-var queryType = new graphql.GraphQLObjectType({  
+let MutationAdd = {
+  type: userType,
+  descripton: `add user`,
+  args: {
+    title: {
+      name: 'user email',
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+    }
+  },
+  resolve: ( root, args ) => {
+    let newUser = new USER({
+      email: args.title,
+      password: 'true'
+    })
+    newUser.id = newUser._id
+    return new Promise((resolve, reject) => {
+      newUser.save(function (err) {
+        if (err) reject(err)
+          else resolve(newUser)
+      })
+    })
+  }
+}
+
+const MutationType = new graphql.GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    add: MutationAdd
+  }
+});
+
+const queryType = new graphql.GraphQLObjectType({
   name: 'Query',
-  fields: () => ({
-    lists: {
-      type: new graphql.GraphQLList( listType ),
-      resolve: () => {
-        return new Promise((resolve, reject) => {
-          List.find((err, lists) => {
-            if (err) reject(err)
-            else resolve(lists)
-          })
-        })
+  fields: function () {
+    return {
+      users: {
+        type: new graphql.GraphQLList(userType),
+        resolve: function () {
+          return new Promise( function(resolve, reject) {
+            setTimeout(function () {
+              resolve(USERS);
+            }, 4000)
+          } );
+        }
       }
     }
-  })
+  }
 })
 
 
-module.exports = new graphql.GraphQLSchema({  
-  query: queryType
-});
+
+
+
+
+module.exports = new graphql.GraphQLSchema({
+  query: queryType,
+  mutation: MutationType
+})
+
+// id: {
+      //   type: graphql.GraphQLID
+      // },
+      // name: {
+      //   type: graphql.GraphQLString
+      // },
+      // address: {
+      //   type: graphql.GraphQLString
+      // },
+      // address2: {
+      //   type: graphql.GraphQLString
+      // },
+      // relocation: {
+      //   type: graphql.GraphQLBoolean
+      // },
+      // age: {
+      //   type: graphql.GraphQLString
+      // },
+      // phone: {
+      //   type: graphql.GraphQLString
+      // },
+      // resume: {
+      //   type: graphql.GraphQLString
+      // },
+      // coverLetter: {
+      //   type: graphql.GraphQLString
+      // },
+      // linkedIn: {
+      //   type: graphql.GraphQLString
+      // },
+      // gitHub: {
+      //   type: graphql.GraphQLString
+      // },
+      // authorized: {
+      //   type: graphql.GraphQLBoolean
+      // },
+      // disability: {
+      //   type: graphql.GraphQLBoolean
+      // }
+
+      // id: {
+      //   type: graphql.GraphQLID
+      // },
+      // email: {
+      //   type: graphql.GraphQLString
+      // },
+      // password: {
+      //   type: graphql.GraphQLString
+      // }
+
+
+// let userType = new graphql.GraphQLObjectType({
+//   name: 'user',
+//   fields: () => ({
+//     id: {
+//         type: graphql.GraphQLID, //may not need or even want id
+//         descripton: "yah bitch yahh",
+//         args: {
+//           name: {
+//             type: graphql.GraphQLString,
+//             descripton:'say hello mf'
+//           }
+//         }
+//       },
+//       email: {
+//         type: graphql.GraphQLString,
+//                 descripton: "yah bitch yahh",
+//         args: {
+//           name: {
+//             type: graphql.GraphQLString,
+//             descripton:'say hello mf'
+//           }
+//         },
+//       resolve: (_,args) => {
+//         return `Hello, ${args.name}!!!`;
+//         }
+//       },
+//       password: {
+//         type: graphql.GraphQLString,
+//                 descripton: "yah bitch yahh",
+//         args: {
+//           name: {
+//             type: graphql.GraphQLString,
+//             descripton:'say hello mf'
+//           }
+//         },
+//       resolve: (_,args) => {
+//         return `Hello, ${args.name}!!!`;
+//       }
+//     }
+//   })
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // var LIST = [  
+// //   {
+// //     "id": 1446412739542,
+// //     "name": "Read emails",
+// //     "age": 23
+// //   },
+// //   {
+// //     "id": 1446412740883,
+// //     "name": "Buy orange",
+// //     "age": 14
+// //   }
+// // ];
+
+// var List = mongoose.model('users',{  
+//   // id: mongoose.Schema.Types.ObjectId,
+//   email: String,
+//   password: String//,
+//    // userInfo: {
+//   //     name: String,
+//   //     address: String,
+//   //     address2: String,
+//   //     relocation: String,
+//   //     age: Number,
+//   //     phone: Number,
+//   //     resume: String,
+//   //     coverLetter: String,
+//   //     linkedIn: String,
+//   //     gitHub: String,
+//   //     authorized: String,
+//   //     disability: Boolean //,
+//   //     // linkToVideo: String
+//   //   }
+// }
+// )
+
+// const userType = new graphql.GraphQLObjectType({
+//   name: 'user',
+//   fields: function(){
+//     return {
+//       id: {
+//         type: graphql.GraphQLID
+//       },
+//       name: {
+//         type: graphql.GraphQLString
+//       },
+//       address: {
+//         type: graphql.GraphQLString
+//       },
+//       address2: {
+//         type: graphql.GraphQLString
+//       },
+//       relocation: {
+//         type: graphql.GraphQLBoolean
+//       },
+//       age: {
+//         type: graphql.GraphQLString
+//       },
+//       phone: {
+//         type: graphql.GraphQLString
+//       },
+//       resume: {
+//         type: graphql.GraphQLString
+//       },
+//       coverLetter: {
+//         type: graphql.GraphQLString
+//       },
+//       linkedIn: {
+//         type: graphql.GraphQLString
+//       },
+//       gitHub: {
+//         type: graphql.GraphQLString
+//       },
+//       authorized: {
+//         type: graphql.GraphQLBoolean
+//       },
+//       disability: {
+//         type: graphql.GraphQLBoolean
+//       }
+//     }
+//   }
+// });
+
+// //defining a data type for each attribute
+// const listType = new graphql.GraphQLObjectType({
+//   name: 'list',
+//   fields: function(){
+//     return {
+//       id: {
+//         type: graphql.GraphQLID
+//       },
+//       email: {
+//         type: graphql.GraphQLString
+//       },
+//       password: {
+//         type: graphql.GraphQLString
+//       }//,
+//       // userInfo: {
+//       //   type: new graphql.GraphQLList(userType)//,
+//       //   // resolve(user) {
+//       //     return [
+//       //       userType//{}
+//       //     ]
+//       //   // }
+//       // }
+  
+//     }
+//   }
+// });
+
+
+// // const userType = new graphql.GraphQLObjectType({
+// //   name: 'User',
+// //   fields: () => ({
+// //     lists: {
+// //       type: new graphql.GraphQLList( userType ),
+// //       resolve: () => {
+// //         return new Promise((resolve, reject) => {
+// //           List.find((err, user) => {
+// //             if (err) reject(err)
+// //             else resolve(user)
+// //           })
+// //         })
+// //       }
+// //     }
+// //   })
+// // })
+
+// var queryType = new graphql.GraphQLObjectType({  
+//   name: 'Query',
+//   fields: () => ({
+//     lists: {
+//       type: new graphql.GraphQLList( listType ),
+//       resolve: () => {
+//         return new Promise((resolve, reject) => {
+//           List.find((err, lists) => {
+//             if (err) reject(err)
+//             else resolve(lists)
+//           })
+//         })
+//       }
+//     }
+//   })
+// })
+
+
+// module.exports = new graphql.GraphQLSchema({  
+//   query: queryType
+// });
+
+
