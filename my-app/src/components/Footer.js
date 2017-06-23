5fb4fadf16844eeaa54a92b58ff907cfbab05e00
BrandonLRobinson1 <BrandonLRobinson1@gmail.com>
2017-06-22 22:08:23 -0400

    fixed link issue

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -5,13 +5,30 @@ class Footer extends React.Component{
 
   render(){
     console.log(this.props)
-    return (
+    return ( 
       <footer className="App">
-          <h4>footer</h4>
-          <button onClick={()=>{this.props.switch()}}>change state</button>
+        
       </footer>
     )
   }
 }
 
 export default Footer;
+
+// import React from 'react';
+// // import '../App.css';
+
+// class Footer extends React.Component{
+
+//   render(){
+//     console.log(this.props)
+//     return ( 
+//       <footer className="App">
+//           <h4>footer</h4>
+//           <button onClick={()=>{this.props.switch()}}>change state</button>
+//       </footer>
+//     )
+//   }
+// }
+
+// export default Footer;
